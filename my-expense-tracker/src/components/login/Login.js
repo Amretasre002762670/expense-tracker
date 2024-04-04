25276218cf5222c1a8518ebcc155e8fb96400a54
amretasrerengarajan <rengarajanthiruven.a@northeastern.edu>
2024-04-04 00:01:13 -0400

    Changed the theme and styling for login

--- my-expense-tracker/src/components/login/Login.js
+++ my-expense-tracker/src/components/login/Login.js
@@ -4,14 +4,20 @@ import '../login/Login.css';
 const Login = () => {
     const [emailError, setEmailError] = useState('');
     const [passwordError, setPasswordError] = useState('');
+    const [emailErrorText, setEmailErrorText] = useState('');
 
     const handleEmailValidation = (event) => {
         const email = event.target.value;
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if (!emailPattern.test(email)) {
+        if (!email.trim()) {
+            setEmailErrorText("Email cannot be empty");
             setEmailError(true);
-        } else {
+        } else if (!emailPattern.test(email)) {
+            setEmailErrorText("Invalid Email");
+            setEmailError(true);
+        }
+        else {
             setEmailError(false);
         }
     };
@@ -35,30 +41,35 @@ const Login = () => {
                         <form>
                             <div className="inside-container">
                                 <div className="row g-3 align-items-center email-row">
-                                    <div className="col-3">
-                                        <label for="email" className="col-form-label">Email</label>
+                                    <div className="col-4">
+                                        <label for="email" className="col-form-label">Email:</label>
                                     </div>
                                     <div className="col">
-                                        <input type="text" id="email" className="form-control" onBlur={handleEmailValidation} />
+                                        <input type="text" id="email" className="form-control input-text-box" onBlur={handleEmailValidation} />
                                     </div>
                                 </div>
                                 <div className="danger-row">
-                                    {emailError ? <div className="text-danger email-error"> Invalid email format </div> : <div className="place-holder"></div>}
+                                    {emailError ? <div className="text-danger error-message"> {emailErrorText} </div> : <div className="place-holder"></div>}
                                 </div>
                                 <div className="row g-3 align-items-center password-row">
-                                    <div className="col-3">
-                                        <label for="password" className="col-form-label">Password</label>
+                                    <div className="col-4">
+                                        <label for="password" className="col-form-label">Password:</label>
                                     </div>
                                     <div className="col">
-                                        <input type="password" id="password" className="form-control" aria-describedby="passwordHelpInline" onBlur={handlePasswordValidation} />
+                                        <input type="password" id="password" className="form-control input-text-box" aria-describedby="passwordHelpInline" onBlur={handlePasswordValidation} />
                                     </div>
                                 </div>
                                 <div className="danger-row">
-                                    {passwordError ? <div className="text-danger password-error">Password cannot be empty</div> : <div className="place-holder"></div>}
+                                    {passwordError ? <div className="text-danger error-message">Password cannot be empty</div> : <div className="place-holder"></div>}
+                                </div>
+                                <div className="row g-3 link-row align-items-center">
+                                    <a href="/signup" className="signup-link">Sign Up Instead</a>
                                 </div>
                                 <div className="row g-3 button-row align-items-center">
                                     <button type="submit" className="btn btn-primary">Login</button>
                                 </div>
+                                
+
                             </div>
 
                         </form>
